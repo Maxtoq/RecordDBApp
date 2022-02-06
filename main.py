7579aba76bc8805be38f5ae1922ed3b097f881eb
Maxtoq <maxime.toquebiau@hotmail.fr>
2022-02-06 23:37:15 +0100

    continue

--- main.py
+++ main.py
@@ -5,23 +5,29 @@ from kivy.lang import Builder
 from kivymd.uix.list import TwoLineListItem
 
 class Main(MDApp):
+    data = {
+        'Add Record': 'album'
+    }
 
     def build(self):
         return Builder.load_file("main_page.kv")
 
     def on_start(self):
         with open("Records.json") as f:
-            tracks_list = json.load(f)["record_collection"]["records"]
+            self.tracks_list = json.load(f)["record_collection"]["records"]
 
-        for track in tracks_list:
+        for track in self.tracks_list:
             track = track["record"]
-            print(track)
             self.root.ids.track_container.add_widget(
                 TwoLineListItem(
                     text=f"{track['record']} - {track['num']}. {track['track']}",
                     secondary_text=f"{track['artist']}, {track['style']}, BPM={track['bpm']}"
                 )
             )
+
+    def callback(self, instance):
+        if instance.icon == "album":
+            return
 
 
 Main().run()
