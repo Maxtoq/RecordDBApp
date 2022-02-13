a325aa63b158d1821d688a2e5519a0ba2864652c
Maxtoq <maxime.toquebiau@hotmail.fr>
2022-02-13 12:31:43 +0100

    update db, staart adding new track

--- main.py
+++ main.py
@@ -1,33 +1,171 @@
 import json
 
-from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivymd.app import MDApp
 from kivymd.uix.list import TwoLineListItem
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 
-class Main(MDApp):
+
+# TODO:
+# - Add a track
+# - Remove a track
+# - Modify a track
+# - Sort tracks by record
+# - Sort tracks by any attribute
+# - Filter tracks by any attribute
+# - 
+
+
+class RecordDB():
+    def __init__(self, db_file):
+        self.track_list = self.load(db_file)
+
+    def load(self, db_file):
+        with open(db_file) as f:
+            track_list = json.load(f)["tracks_db"]
+        return track_list
+
+    def print_db(self):
+        for track in self.track_list:
+            print(track)
+
+    def add(self, track):
+        # TODO
+        pass
+
+
+class MainPage(Screen):
+
+    def __init__(self, db, **kwargs):
+        super(MainPage, self).__init__()
+        self.db = db
+        self.update_list()
+
+    def update_list(self):
+        # TODO: find a way to do this without removing all items and adding them again
+        # Remove all items
+
+        # Add new items
+        for track in self.db.track_list:
+            style_str = ', '.join(track["style"])
+            self.ids.track_container.add_widget(
+                TwoLineListItem(
+                    text=f"{track['record']} - {track['num']}. {track['track']}",
+                    secondary_text=f"{track['artist']}, {style_str}, BPM={track['bpm']}"
+                )
+            )
+
+        # Sort
+
+
+class AddTrackPage(Screen):
+    confirm_dialog = None
+
+    def __init__(self, app, **kwargs):
+        super(AddTrackPage, self).__init__()
+        self.app = app
+
+    def add_track(self):
+        """ Called when pushing the 'ADD' button. 
+            Ask for confirmation and sends the new track to the main app to be
+            added.
+        """
+        # Create list of style, adding only non-empty styles
+        style_list = []
+        if self.ids.style1.text != '':
+            style_list.append(self.ids.style1.text)
+        if self.ids.style2.text != '':
+            style_list.append(self.ids.style2.text)
+        if self.ids.style3.text != '':
+            style_list.append(self.ids.style3.text)
+        # Create new track dictionary entry
+        new_track = {
+            "record": self.ids.record_name.text,
+            "num": self.ids.track_num.text,
+            "track": self.ids.track_name.text,
+            "artist": self.ids.artist.text,
+            "style": style_list,
+            "bpm": self.ids.bpm.text
+        }
+        # Create the string to display in the confirm dialog
+        style_str = ', '.join(new_track["style"])
+        track_str = "Record: {}\nTrack #: {}\nTrack name:{}\nArtist: {}\nStyle: {}\nBPM: {}".format(
+            new_track["record"], new_track["num"], new_track["track"], 
+            new_track["artist"], style_str, new_track["bpm"]
+        )
+        # Create and open dialog
+        self.confirm_dialog = MDDialog(
+            title="Confirm adding this track ?",
+            text=track_str,
+            buttons=[
+                MDFlatButton(
+                    text="Confirm",
+                    theme_text_color="Custom",
+                    text_color=[1, 0.77, 0, 1],
+                    on_release=lambda *args: self.send_new_track(
+                                                    new_track, *args)
+                ),
+                MDFlatButton(
+                    text="Cancel",
+                    on_release=self.close_dialog
+                )
+            ]
+        )
+        self.confirm_dialog.open()
+
+    def close_dialog(self, *args):
+        self.confirm_dialog.dismiss()
+
+    def send_new_track(self, new_track, *args):
+        self.confirm_dialog.dismiss()
+        self.app.add_new_track(new_track)
+
+
+
+class RecordDBApp(MDApp):
     data = {
         'Add Record': 'album'
     }
 
+    def __init__(self):
+        super(RecordDBApp, self).__init__()
+        # DB
+        self.db = RecordDB("Records.json")
+        self.db.print_db()
+        # Screen manager and pages
+        self.sm = ScreenManager(transition=NoTransition())
+        # Pages
+        self.main_page = None
+        self.add_track_page = None
+
     def build(self):
-        return Builder.load_file("main_page.kv")
+        Builder.load_file("main_page.kv")
+        return self.sm
 
     def on_start(self):
-        with open("Records.json") as f:
-            self.tracks_list = json.load(f)["record_collection"]["records"]
-
-        for track in self.tracks_list:
-            track = track["record"]
-            self.root.ids.track_container.add_widget(
-                TwoLineListItem(
-                    text=f"{track['record']} - {track['num']}. {track['track']}",
-                    secondary_text=f"{track['artist']}, {track['style']}, BPM={track['bpm']}"
-                )
-            )
+        self.main_page = MainPage(self.db, name="main_page")
+        self.add_track_page = AddTrackPage(self, name="add_track_page")
+        self.sm.add_widget(self.main_page)
+        self.sm.add_widget(self.add_track_page)
+        self.sm.current = "main_page"
 
     def callback(self, instance):
+        # Main page: add record button -> go to add record page
         if instance.icon == "album":
-            return
+            self.sm.current = "add_track_page"
+        # Add record page: back button -> go to main page
+        if instance.icon == "arrow-left":
+            self.sm.current = "main_page"
+
+    def add_new_track(self, new_track):
+        # Add track to db
+        self.db.add(new_track)
+        # Update main page list
+        self.main_page.update_list()
+        # Go to main page
+        self.sm.current = "main_page"
 
 
-Main().run()
+RecordDBApp().run()
